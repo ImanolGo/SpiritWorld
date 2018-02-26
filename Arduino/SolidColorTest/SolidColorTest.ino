d05c8efb63299001a245b0cce0f1a500ab15c7dd
Imanol Gomez <yo@imanolgomez.net>
2018-02-26 22:46:49 +0100

    first commit

---
+++ Arduino/SolidColorTest/SolidColorTest.ino
@@ -0,0 +1,39 @@
+#include <FastLED.h>
+
+#define DATA_PIN_1    27
+#define DATA_PIN_2    18
+#define NUM_LEDS    300
+#define BRIGHTNESS  64
+#define LED_TYPE    WS2812B
+#define COLOR_ORDER GRB
+CRGB leds1[NUM_LEDS];
+CRGB leds2[NUM_LEDS];
+int ledPin = 4;
+
+
+
+void setup() {
+
+    pinMode(ledPin, OUTPUT);
+    delay( 3000 ); // power-up safety delay
+    FastLED.addLeds<LED_TYPE, DATA_PIN_1, COLOR_ORDER>(leds1, NUM_LEDS).setCorrection( TypicalLEDStrip );
+    FastLED.addLeds<LED_TYPE, DATA_PIN_2, COLOR_ORDER>(leds2, NUM_LEDS).setCorrection( TypicalLEDStrip );
+  
+    //FastLED.setBrightness(  BRIGHTNESS );
+    FastLED.setMaxPowerInVoltsAndMilliamps (5, 3100);
+    
+    fill_solid(leds1,NUM_LEDS, CRGB::White);
+    fill_solid(leds2,NUM_LEDS, CRGB::White);
+    FastLED.show();
+
+    digitalWrite(ledPin, HIGH); 
+    
+}
+
+
+void loop()
+{
+   fill_solid(leds1,NUM_LEDS, CRGB::White);
+   fill_solid(leds2,NUM_LEDS, CRGB::White);
+   FastLED.show();
+}
